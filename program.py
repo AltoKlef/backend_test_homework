ab6f73bf89893ef78016858ab3d764d978aa7192
Ruslan <garridubua@gmail.com>
2024-10-24 12:48:31 +0300

    Ваше сообщение коммита

--- program.py
+++ program.py
@@ -1,1 +1,12 @@
-print('Я домашка')
+class Phone:
+
+    line_type = 'проводной'
+
+    def __init__(self):
+        self.dial_type = 'дисковый'
+
+
+# Создать объект rotary_phone.
+rotary_phone = Phone()
+# Создать объект rotary_phone_2.
+rotary_phone_2 = Phone() 
